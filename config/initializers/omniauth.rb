fa7dcc4369cb4b6708d681e596ad13d847c80f95
Zach Latta <zach@zachlatta.com>
2025-02-04 00:47:31 +0000

    Try moving Omniauth to after_initialize to allow prod build

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,11 @@
 # Allow both GET and POST for auth callback
 OmniAuth.config.allowed_request_methods = [:post, :get]
 
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, Rails.application.credentials.github.client_id, Rails.application.credentials.github.client_secret,
-    scope: 'repo,read:org,read:user'
+Rails.application.config.after_initialize do
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :github,
+      Rails.application.credentials.github.client_id,
+      Rails.application.credentials.github.client_secret,
+      scope: 'repo,read:org,read:user'
+  end
 end 
