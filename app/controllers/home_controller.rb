374e8ba654cc70936f4a7ebd9cd8e869b09902d1
Zach Latta <zach@zachlatta.com>
2025-02-04 00:24:12 +0000

    Style

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
     @access_tokens_count = AccessToken.count
+    
+    if latest_token = AccessToken.order(created_at: :desc).first
+      @latest_donor = latest_token.username
+      @latest_donation_time = latest_token.created_at
+    end
   end
 end 
