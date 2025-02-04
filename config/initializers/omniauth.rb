35e644ee241b4a6337e1c9ab70df39bfdaf8456f
Zach Latta <zach@zachlatta.com>
2025-02-04 00:55:54 +0000

    Conditionally load GH credentials to prevent precompile failure

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,11 +1,11 @@
 # Allow both GET and POST for auth callback
 OmniAuth.config.allowed_request_methods = [:post, :get]
 
-Rails.application.config.after_initialize do
+if Rails.application.credentials.github.present?
   Rails.application.config.middleware.use OmniAuth::Builder do
     provider :github,
       Rails.application.credentials.github.client_id,
       Rails.application.credentials.github.client_secret,
       scope: 'repo,read:org,read:user'
   end
-end 
+end
