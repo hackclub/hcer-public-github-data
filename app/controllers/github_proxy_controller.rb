0a5e9492a983aeb8717a1db4ebeed23107b109c9
Zach Latta <zach@zachlatta.com>
2025-02-05 15:41:47 +0000

    Add response caching

--- app/controllers/github_proxy_controller.rb
+++ app/controllers/github_proxy_controller.rb
@@ -2,36 +2,30 @@ class GithubProxyController < ApplicationController
   before_action :authenticate_request
   rescue_from StandardError, with: :handle_error
 
+  CACHE_VERSION = 'v1'
+  CACHE_EXPIRATION = 1.days
+
   def proxy
     path = request.path.sub('/gh/', '')
     query_string = request.query_string.present? ? "?#{request.query_string}" : ''
     full_path = "#{path}#{query_string}"
     
-    api_type = if path.start_with?('search')
-                 :search
-               elsif path.start_with?('graphql')
-                 :graphql
-               else
-                 :core
-               end
+    api_type = determine_api_type(path)
+    cache_key = generate_cache_key(full_path, api_type)
+    
+    response = Rails.cache.fetch(cache_key, expires_in: CACHE_EXPIRATION) do
+      token = AccessToken.find_available_token(api_type)
+      raise 'No available tokens' unless token
 
-    token = AccessToken.find_available_token(api_type)
-    raise 'No available tokens' unless token
-
-    response = token.with_lock do
-      resp = token.client.get(full_path)
-
-      token.assign_attributes(
-        "#{api_type}_rate_limit_remaining" => token.client.rate_limit.remaining,
-        "#{api_type}_rate_limit_reset_at" => token.client.rate_limit.resets_at
-      )
-      token.save!
-
-      resp.to_hash
-    rescue Octokit::Error => e
-      token.assign_rate_limits_from_api
-      token.save!
-      raise 
+      token.with_lock do
+        resp = token.client.get(full_path)
+        update_token_rate_limits(token, api_type)
+        resp.to_hash
+      rescue Octokit::Error => e
+        token.assign_rate_limits_from_api
+        token.save!
+        raise 
+      end
     end
 
     render json: response
@@ -52,5 +46,33 @@ class GithubProxyController < ApplicationController
       status: error.is_a?(Octokit::Error) ? error.response_status : 500
     }
     render json: error_response, status: error_response[:status]
+  end
+
+  def determine_api_type(path)
+    if path.start_with?('search')
+      :search
+    elsif path.start_with?('graphql')
+      :graphql
+    else
+      :core
+    end
+  end
+
+  def generate_cache_key(full_path, api_type)
+    key_parts = [
+      'github_proxy',
+      CACHE_VERSION,
+      api_type,
+      Digest::SHA256.hexdigest(full_path)
+    ]
+    key_parts.join('/')
+  end
+
+  def update_token_rate_limits(token, api_type)
+    token.assign_attributes(
+      "#{api_type}_rate_limit_remaining" => token.client.rate_limit.remaining,
+      "#{api_type}_rate_limit_reset_at" => token.client.rate_limit.resets_at
+    )
+    token.save!
   end
 end 
